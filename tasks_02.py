1874be6b40500cdcb557aa3a165ee7b1696cbccd
veneta13 <venkireva80@gmail.com>
2025-03-20 23:14:21 +0200

    Add list task

---
+++ tasks_02.py
@@ -0,0 +1,27 @@
+########### Problem 1
+# You are a librarian, and you need to perform various
+# operations on the list of books.
+
+# Initialize the Library: Start with an empty list of books.
+
+# Add the books "Harry Potter", "1984" and "The Great Gatsby" to the collection.
+
+# Remove "Harry Potter" from the collection.
+
+# Sort the books alphabetically.
+
+# Check if "Harry Potter" is in the library.
+
+# Check if "Romeo and Juliet" is in the collection.
+
+# Count the total number of books.
+
+# Insert "Alice in Wonderland" at the appropriate position.
+
+# Remove and return the last (alphabetically) book.
+
+# Find the index of "The Great Gatsby".
+
+# Reverse the order of the books.
+
+# Remove all books from the collection.
