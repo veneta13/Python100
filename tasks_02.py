d9f7bef8972cb36a978303c0338d5c21ed21b704
Veneta <venkireva80@gmail.com>
2025-03-21 17:40:07 +0200

    Add iterable tasks

--- tasks_02.py
+++ tasks_02.py
@@ -1,4 +1,5 @@
 ########### Problem 1
+
 # You are a librarian, and you need to perform various
 # operations on the list of books.
 
@@ -22,6 +23,78 @@
 
 # Find the index of "The Great Gatsby".
 
+# Create a new library, excluding the first book of the current one.
+
+# Create a new library, including only second and the third book of the current one.
+
 # Reverse the order of the books.
 
 # Remove all books from the collection.
+
+
+########### Problem 2
+
+# Use a lambda function with map to square each number in the list.
+# Expected output: numbers = [1, 2, 3, 4, 5]
+numbers = [1, 2, 3, 4, 5]
+
+
+# Use a lambda function to add corresponding elements from the two lists.
+# Expected output: [5, 7, 9]
+my_list_1 = [1, 2, 3]
+my_list_2 = [4, 5, 6]
+
+# Use a lambda function with map to calculate the length of each string.
+# Expected output: [6, 5, 10]
+list_of_strings = ["banana", "apple", "strawberry"]
+
+
+########### Problem 3
+
+# Create a tuple with the the fellowing fruits: "banana", "apple", "strawberry", "cherry"
+
+# Print the name of the second fruit in the tuple.
+
+# Count how many times "apple" appears in the tuple.
+
+# Find the index of "banana" in the tuple.
+
+# Create a new tuple containing the first three fruits.
+
+
+# ########### Problem 4
+
+# Create a dictionary with student names and grades: 'Alice': 85, 'Bob': 92, 'Charlie': 78
+
+# Print Bob's grade
+
+# Add a new student Peter and their grade
+
+# Remove 'Alice' from the class
+
+# Check if there is a student named 'Charlie'
+
+# Print a list of all student names
+
+# Print a list of all grades
+
+# Add 2 new students - Alex and Rob, and their grades - 93 and 76
+
+
+# ########### Problem 5
+
+# Create a set with the movies "Inception", "The Matrix", "Interstellar", "The Dark Knight"
+
+# Add the movie "Avatar" movie to the set
+
+# Remove "Interstellar" from the set
+
+# Check if 'Interstellar' is in the set
+
+# Create another set with a friend's favorite movies: "Inception", "The Matrix", "The Lord of the Rings"
+
+# Print the union of the two sets
+
+# Print the intersection of the two sets
+
+# Print the difference of the two sets
